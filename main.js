c410bc1c337a7a09530a60902c248e0bd6d80c46
emily-boiston <82091625+emily-boiston@users.noreply.github.com>
2021-05-20 13:49:50 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+alert("hello world");
+window.alert("hello world");
+document.write('hello world');
+var x = "i like jello"
+document.write(x) ;
+var a="this is a string";
+document.write(a);
+document.write("lisa told bart,\"knock it off bart! or i'll tell dad!\"<br>
+\"eat my shorts!\"bart responded.");
+ var b =" concatenated"+"string"
+ document.write(b)
+ var blues="i have the blues";
+ var blues="blues.fontcolor("blue");
+ document.write(blues);
+ document.write(3+3)
+ function my_first_function() {    //defining function//
+	 var str="this text is green";  //coloring it//
+	 var result=="str.font color("green");
+	 document.getelementbyid("green_text").innerhtml = result;  //final result//
+ }
