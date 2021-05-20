5746e8a860074326ac0bee0584afcaa7d8947e94
emily-boiston <82091625+emily-boiston@users.noreply.github.com>
2021-05-20 14:24:19 -0700

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,10 @@
+function my_function() {
+    var str="video studio makes me mad"; //tried to make a button didnt work
+    var result="str.font color"("red"); //tryed to make it red idnt work//
+    document.getElementById("red_text").innerHTML = result;
+}
+window.alert("mad rant ahaid"); //somehow this works//
+var mad="mad";
+var mad="mad.fontcolor"("red"); ///this somehow dosent work//
+document.write(mad)
+document.write(1+3=4)
