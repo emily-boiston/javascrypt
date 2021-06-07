3ce6c0046bab0a6930dc3ead06790492ce98ab71
emily-boiston <82091625+emily-boiston@users.noreply.github.com>
2021-06-07 14:52:15 -0700

    Add files via upload

---
+++ mathmatics.js
@@ -0,0 +1,37 @@
+finction addition_function() {
+    var addition = 2+2;
+document.getElementById("math").innerHTML ="2+2="+addition;
+}
+var p = document.getElementById("math");
+function subtraction_function() {
+	var subtraction="5-2;
+	document.getElementById("Math").innerHTML="5-2="+subtracxtion;
+}
+function multiplication() {
+	var simple_Math=6*8;
+	document.getElementById("Math").innerHtml="6x8=" +simple_Math;
+}
+function division() {
+	var simple_Math=48/6;
+	document.getElementById("Math").innerHtml="48/6=" +simple_Math;
+}
+function more_math() {
+	var simple_math=(1=2)*10/2-5;
+	document.getElemntById("Math").innerHtml="1 plus 2,multiplied by 10,divided in halfand then subtracted by 5 equals"+simple_math;
+}
+function modulus_operator() {
+	var simple_Math= 25%6;
+	document.getElementById("Math").innerHtml="when you divide 25 by 6 you have a remainder of:"+simple_Math;
+}
+function megation_operatior() {
+	var x=10;
+	document.getElementById("math").innerHTML=-x;
+}
+var x=5;
+x++;
+document.write(x);
+var x=5.25;
+x--;
+document.write(x);
+window.allert(math.random());
+window.alert(math.random()*100);
